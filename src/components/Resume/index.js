2defdb16a7d4310e749ef2448a3388db68a3e9c1
Tocopro <nicwac200g@mail.com>
2023-11-06 21:28:34 -0600

    revised code

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -7,7 +7,7 @@ function Resume() {
 				<h1 className="page-header">My Resume</h1>
 			</div>
 			<div className="bottom-spacing">
-				<a href={require("../../assets/files/Owais' Resume.pdf")} download>
+				<a href={require("../../assets/files/https://www.dropbox.com/home")} download>
 					<h4>To Download my Resume Click here</h4>
 				</a>
 			</div>
